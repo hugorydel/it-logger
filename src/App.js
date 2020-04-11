f18ec4a5ec08ee9eea69e46c296994ec2c7bace8
Hugo Rydel <hugosteczkowski@gmail.com>
2020-04-11 17:46:59 -0400

    Adding Icons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+//This brings in the main css
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    //Initializing Materialize JavaScript
+    //Now we can use modals and all that
+    M.AutoInit();
+  });
   return <div className='App'>My Application</div>;
 };
 
